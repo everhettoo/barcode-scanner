2402dd1a0d11c7c598a3650041363d082803629d
everhettoo <everhettoo@gmail.com>
2025-05-29 15:39:31 +0800

    Added morphology nb for cross SE

---
+++ utility/notebook_helper.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from ipcv import cvlib
+
+
+def display_for_comparisons(prev_img, curr_img, prev_label, curr_label):
+    # Display the previous and current images side-by-side for visual comparison.
+    plt.figure(figsize=(14, 12))
+    plt.subplot(1, 2, 1)
+    plt.imshow(prev_img, cmap='gray')
+    plt.title(prev_label)
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(curr_img, cmap='gray')
+    plt.title(curr_label)
+
+
+def display_color_grids(binary_array):
+    cmap = colors.ListedColormap(['white', 'gray'])
+
+    fig, ax = plt.subplots()
+    ax.imshow(binary_array, cmap=cmap, origin='upper', extent=[0, binary_array.shape[1], 0, binary_array.shape[0]])
+    ax.grid(color='green', linewidth=1)
+    ax.set_xticks(np.arange(0, binary_array.shape[1], 1))
+    ax.set_yticks(np.arange(0, binary_array.shape[0], 1))
+    plt.show()
